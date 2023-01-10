2308aa76f7dd24798c8756dbb436d410d8ff665a
Josh <eppersonjoshua435@gmail.com>
2023-01-09 16:43:52 -0800

    working drivetrain with simulation

---
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -0,0 +1,196 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.InvertType;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
+import com.ctre.phoenix.motorcontrol.TalonFXSimCollection;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+import edu.wpi.first.math.estimator.DifferentialDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.simulation.ADXRS450_GyroSim;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.DuckGearUtil;
+import frc.robot.RobotContainer;
+
+public class DrivetrainSubsystem extends SubsystemBase {
+  private final DifferentialDrive robotDrive;
+  private final DifferentialDrivetrainSim robotDriveSim;
+
+  // CAN devices
+  private final WPI_TalonFX MainLeftMotorBack = new WPI_TalonFX(Constants.CAN.Drivetrain.BL);
+  private final WPI_TalonFX MainRightMotorBack = new WPI_TalonFX(Constants.CAN.Drivetrain.BR);
+  private final WPI_TalonFX MainLeftMotorFront = new WPI_TalonFX(Constants.CAN.Drivetrain.FL);
+  private final WPI_TalonFX MainRightMotorFront = new WPI_TalonFX(Constants.CAN.Drivetrain.FR);
+
+  private final TalonFXSimCollection leftMotorSim;
+  private final TalonFXSimCollection rightMotorSim;
+  
+  private final ADXRS450_Gyro gyro = new ADXRS450_Gyro();
+  private final ADXRS450_GyroSim gyroSim = new ADXRS450_GyroSim(gyro);
+
+  private final DifferentialDrivePoseEstimator odometry = new DifferentialDrivePoseEstimator(
+    new DifferentialDriveKinematics(Constants.DrivetrainCharacteristics.trackWidthMeters), 
+    Rotation2d.fromDegrees(-gyro.getAngle()), 
+    0,0,
+    new Pose2d()); //will add vision measurements once auton starts;
+
+  private final Field2d field;
+  public DrivetrainSubsystem() {
+    //constructor gets ran at robotInit()
+    gyro.calibrate();
+        
+    // Reset settings
+    MainLeftMotorBack.configFactoryDefault();
+    MainRightMotorBack.configFactoryDefault();
+    MainLeftMotorFront.configFactoryDefault();
+    MainRightMotorFront.configFactoryDefault();
+
+    // Setup the integrated sensor
+    MainLeftMotorBack.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0, 100);
+    MainRightMotorBack.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0, 100);
+
+    // Slave the front motors to their respective back motors
+    MainLeftMotorFront.follow(MainLeftMotorBack);
+    MainRightMotorFront.follow(MainRightMotorBack);
+
+    // Disable voltage compensation, it's bad to be compensating voltage for a
+    // system which draws loads of amps
+    MainLeftMotorBack.enableVoltageCompensation(false);
+    MainLeftMotorFront.enableVoltageCompensation(false);
+    MainRightMotorBack.enableVoltageCompensation(false);
+    MainRightMotorFront.enableVoltageCompensation(false);
+
+    //Apply brake mode to prevent robot from hitting people if watchdog fails
+    MainLeftMotorBack.setNeutralMode(NeutralMode.Brake);
+    MainLeftMotorFront.setNeutralMode(NeutralMode.Brake);
+    MainRightMotorBack.setNeutralMode(NeutralMode.Brake);
+    MainRightMotorFront.setNeutralMode(NeutralMode.Brake);
+
+    // Invert one of the sides
+    MainLeftMotorBack.setInverted(true);
+    MainRightMotorBack.setInverted(false);
+    MainLeftMotorFront.setInverted(InvertType.FollowMaster);
+    MainRightMotorFront.setInverted(InvertType.FollowMaster);
+
+    robotDrive = new DifferentialDrive(MainLeftMotorBack, MainRightMotorBack);
+    robotDriveSim = new DifferentialDrivetrainSim(
+        DCMotor.getFalcon500(2),
+        Constants.DrivetrainCharacteristics.gearing,
+        2.1, // made up number
+        100, // made up number
+        Constants.DrivetrainCharacteristics.wheelRadiusMeters,
+        Constants.DrivetrainCharacteristics.trackWidthMeters,
+        null
+    );
+    field = new Field2d();
+    SmartDashboard.putData("Field", field);
+
+    leftMotorSim = MainLeftMotorBack.getSimCollection();
+    rightMotorSim = MainRightMotorBack.getSimCollection();
+  }
+
+  @Override
+  public void periodic() {
+    double leftDistanceMeters = DuckGearUtil.encoderTicksToMeters(MainLeftMotorBack.getSelectedSensorPosition(),
+    Constants.DrivetrainCharacteristics.gearing, 2048.0, Constants.DrivetrainCharacteristics.wheelRadiusMeters);
+    double rightDistanceMeters = DuckGearUtil.encoderTicksToMeters(MainLeftMotorBack.getSelectedSensorPosition(),
+    Constants.DrivetrainCharacteristics.gearing, 2048.0, Constants.DrivetrainCharacteristics.wheelRadiusMeters);
+
+    odometry.update(gyro.getRotation2d(), leftDistanceMeters, rightDistanceMeters);
+
+    double x = -RobotContainer.driverJoystick.getRawAxis(Constants.DriverConstants.ForwardDriveAxis);
+    double y = RobotContainer.driverJoystick.getRawAxis(Constants.DriverConstants.TurningDriveAxis);
+
+    // extremize
+    double tempY = y;
+    y = Math.signum(x) * (Math.abs(x) < Constants.DriverConstants.deadbandLeftJoystick ? 0 : 1);
+    x = Math.signum(tempY) * (Math.abs(tempY) < Constants.DriverConstants.deadbandRightJoystick ? 0 : 1);
+
+    if (y != 0) {
+        robotDrive.arcadeDrive(y, -x);
+    } else {
+        robotDrive.tankDrive(x, -x);
+    }
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    Pose2d robotPose = odometry.getEstimatedPosition();
+    SmartDashboard.putString("pose", robotPose.toString());
+    field.setRobotPose(robotPose);
+    // For the motor master which is inverted, you'll need to invert it manually (ie
+    // with a negative sign) here when fetching any data
+    // CTRE doesn't support setInverted() for simulation
+
+    leftMotorSim.setBusVoltage(RobotController.getBatteryVoltage());
+    rightMotorSim.setBusVoltage(RobotController.getBatteryVoltage());
+    robotDriveSim.setInputs(leftMotorSim.getMotorOutputLeadVoltage(),
+        -rightMotorSim.getMotorOutputLeadVoltage());
+    // The roboRIO updates at 50hz so you want to match what it actually is in
+    // simulation to get accurate simulations
+    robotDriveSim.update(0.02);
+
+    // Update sensors
+    leftMotorSim
+        .setIntegratedSensorRawPosition((int) DuckGearUtil.metersToEncoderTicks(robotDriveSim.getLeftPositionMeters(),
+            Constants.DrivetrainCharacteristics.gearing, 2048.0, Constants.DrivetrainCharacteristics.wheelRadiusMeters));
+    leftMotorSim.setIntegratedSensorVelocity(
+        (int) DuckGearUtil.metersPerSecondToEncoderTicksPer100ms(robotDriveSim.getLeftVelocityMetersPerSecond(),
+        Constants.DrivetrainCharacteristics.gearing, 2048.0, Constants.DrivetrainCharacteristics.wheelRadiusMeters));
+    rightMotorSim
+        .setIntegratedSensorRawPosition((int) DuckGearUtil.metersToEncoderTicks(-robotDriveSim.getRightPositionMeters(),
+        Constants.DrivetrainCharacteristics.gearing, 2048.0, Constants.DrivetrainCharacteristics.wheelRadiusMeters));
+    rightMotorSim.setIntegratedSensorVelocity(
+        (int) DuckGearUtil.metersPerSecondToEncoderTicksPer100ms(-robotDriveSim.getRightVelocityMetersPerSecond(),
+        Constants.DrivetrainCharacteristics.gearing, 2048.0, Constants.DrivetrainCharacteristics.wheelRadiusMeters));
+
+    // Update simulation gyro, it's detached from the actual gyro
+    gyroSim.setAngle(robotDriveSim.getHeading().getDegrees());
+  }
+  public DifferentialDriveWheelSpeeds getWheelSpeeds() {
+    return new DifferentialDriveWheelSpeeds(
+        DuckGearUtil.EncoderTicksPer100msToMetersPerSecond(MainLeftMotorBack.getSelectedSensorVelocity(),
+            Constants.DrivetrainCharacteristics.gearing, 2048.0, Constants.DrivetrainCharacteristics.wheelRadiusMeters),
+        DuckGearUtil.EncoderTicksPer100msToMetersPerSecond(MainRightMotorBack.getSelectedSensorVelocity(),
+            Constants.DrivetrainCharacteristics.gearing, 2048.0, Constants.DrivetrainCharacteristics.wheelRadiusMeters));
+  }
+
+  public Pose2d getPose() {
+    return odometry.getEstimatedPosition();
+  }
+
+  public void tankDriveVolts(double leftVolts, double rightVolts) {
+    MainLeftMotorBack.setVoltage(leftVolts);
+    MainRightMotorBack.setVoltage(rightVolts);
+    robotDrive.feed(); // feed watchdog to prevent error from clogging can bus
+  }
+
+  public void resetOdometry(Pose2d pose) {
+    resetEncoders();
+
+    odometry.resetPosition(gyro.getRotation2d(),
+    DuckGearUtil.encoderTicksToMeters(MainLeftMotorBack.getSelectedSensorPosition(),
+    Constants.DrivetrainCharacteristics.gearing, 2048.0, Constants.DrivetrainCharacteristics.wheelRadiusMeters),
+    DuckGearUtil.encoderTicksToMeters(MainLeftMotorBack.getSelectedSensorPosition(),
+    Constants.DrivetrainCharacteristics.gearing, 2048.0, Constants.DrivetrainCharacteristics.wheelRadiusMeters), 
+    pose);
+  }
+
+  public void resetEncoders() {
+    MainLeftMotorBack.setSelectedSensorPosition(0);
+    MainRightMotorBack.setSelectedSensorPosition(0);
+  }
+
+}
