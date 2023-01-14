55878b39e715ca4aafd8c40dff30b8d76a733c8e
1266devilduckies <46945122+1266devilduckies@users.noreply.github.com>
2023-01-13 18:27:40 -0800

    Auto System

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -9,30 +9,13 @@ import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
 import com.pathplanner.lib.auto.RamseteAutoBuilder;
 
-import edu.wpi.first.hal.simulation.ConstBufferCallback;
-import edu.wpi.first.math.controller.RamseteController;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.wpilibj.RobotController;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.RamseteCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
-import frc.robot.Robot;
 import frc.robot.subsystems.DrivetrainSubsystem;
 
 public final class Autos {
   
-public Auto()
-
-  public enum Auto{
-    HIGH_DOCKHIGH,
-    HIGH_DOCKLOW,
-    HIGH_FORWARD(),
-  }
-
-  public static CommandBase templateAuto(DrivetrainSubsystem drivetrainSubsystem) {
+  /*public static CommandBase templateAuto(DrivetrainSubsystem drivetrainSubsystem) {
     PathPlannerTrajectory examplePath = PathPlanner.loadPath("New Path", new PathConstraints(4, 3)); //in terms of m/s
     
     RamseteAutoBuilder autoBuilder = new RamseteAutoBuilder(
@@ -41,15 +24,15 @@ public Auto()
     drivetrainSubsystem.ramseteController, 
     drivetrainSubsystem.drivetrainKinematics, 
     drivetrainSubsystem::tankDriveMetersPerSecond, 
-    Constants.eventMap, 
+    Constants.eventMap,  
     drivetrainSubsystem);
 
     return autoBuilder.fullAuto(examplePath);
-  }
+  }*/
 
   //example auto that drives forward 
-  public static CommandBase driveForwardAuto(DrivetrainSubsystem drivetrainSubsystem) {
-    PathPlannerTrajectory path = PathPlanner.loadPath("ForwardPath", new PathConstraints(4, 3)); //in terms of m/s
+  public static CommandBase runPath(DrivetrainSubsystem drivetrainSubsystem, AutoPath autoPath) {
+    PathPlannerTrajectory path = PathPlanner.loadPath(autoPath.getPathName(), new PathConstraints(4, 3)); //in terms of m/s
     
     RamseteAutoBuilder autoBuilder = new RamseteAutoBuilder(
     drivetrainSubsystem::getPose, 
